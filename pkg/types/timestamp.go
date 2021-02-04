07329aa23184afd4556d97f64c0675a7bed42927
hamalliu <1050809100@qq.com>
2021-02-04 17:56:52 +0800

    init

--- pkg/types/timestamp.go
+++ pkg/types/timestamp.go
@@ -6,8 +6,26 @@ import (
 	"time"
 )
 
+var timeformat = "2006-01-02 15:04:05"
+
 // Timestamp be used to MySql timestamp converting.
 type Timestamp int64
+
+// UnmarshalJSON ...
+func (jt *Timestamp) UnmarshalJSON(data []byte) (err error) {
+	t, err := time.Parse(`"` + timeformat + `"`, string(data))
+	if err != nil {
+		return err
+	}
+	*jt = Timestamp(t.Unix())
+	return nil
+}
+
+// MarshalJSON ...
+func (jt *Timestamp) MarshalJSON() ([]byte, error) {
+	timeStr := time.Unix(int64(*jt), 0).Format(timeformat)
+	return []byte(`"` + timeStr + `"`), nil
+}
 
 // Scan scan time.
 func (jt *Timestamp) Scan(src interface{}) (err error) {
